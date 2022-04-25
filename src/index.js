49af31f92e37cac1882dded9c6c64647341a1e18
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-04-25 09:30:35 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const style = {backgroundColor: 'red', color: 'gray'}
+const main = (
+  <div style={style}>
+    <h1>I'll get there. 😎</h1>
+  </div>
+)
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    {main}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
