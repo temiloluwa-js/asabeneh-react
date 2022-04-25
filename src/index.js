be1921369e7a19bde82319017ed1cdbcd817e905
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-04-25 10:16:45 +0100

    third commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const Age = (props) => <div>The person is {props.age} years old.</div>
-const Weight = (props) => (
-  <p>The weight of the object on Earth is {props.weight} N.</p>
-)
-
-const App = () => {
-  let currentYear = 2020
-  let birthYear = 1820
-  const age = currentYear - birthYear
-  const gravity = 9.81
-  const mass = 75
-
-  return (
-    <div className="app">
-      <Age age={age}/>
-      <Weight weight={gravity * mass}/>
-    </div>
-  )
+const showDate = (time) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May'
+  ]
 }
 
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
