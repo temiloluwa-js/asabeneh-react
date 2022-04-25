dd93f5dc951a096f1a453ad875216fb9e11740df
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-04-25 09:46:31 +0100

    third commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom/client';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const style = {backgroundColor: 'red', color: 'gray'}
-const main = (
-  <div style={style}>
-    <h1>I'll get there. 😎</h1>
-  </div>
+const Age = (props) => <div>The person is {props.age} years old.</div>
+const Weight = (props) => (
+  <p>The weight of the object on Earth is {props.weight} N.</p>
 )
+
+const App = () => {
+  let currentYear = 2020
+  let birthYear = 1820
+  const age = currentYear - birthYear
+  const gravity = 9.81
+  const mass = 75
+
+  return (
+    <div className="app">
+      <Age age={age}/>
+      <Weight weight={gravity * mass}/>
+    </div>
+  )
+}
 
 
 root.render(
   <React.StrictMode>
-    {main}
+    <App/>
   </React.StrictMode>
 );
