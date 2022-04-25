7610617d5e13cd460c84ba336df5e9b56ebaf2bc
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-04-25 11:37:56 +0100

    mapping in React

--- src/index.js
+++ src/index.js
@@ -1,57 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const showDate = (time) => {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  const month = months[time.getMonth()];
-  const year = time.getFullYear();
-  const date = time.getDate();
-  return `${month} ${date}, ${year}`;
-};
-
-const Header = (props) => {
-  const data = props.data;
-  const { welcome, title, subtitle, author, date } = data;
-  const { firstName, lastName } = author;
-  return (
-    <header>
-      <div className="header-wrapper">
-        <h1>{welcome}</h1>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <small>{showDate(date)}</small>
-      </div>
-    </header>
-  );
-};
-
-const App = () => {
-  const data = {
-    welcome: 'Welcome to 30 Days Of React',
-    title: 'Getting started React'
-  }
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Countries countries={countries} />
   </React.StrictMode>
 );
