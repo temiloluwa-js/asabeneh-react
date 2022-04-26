62e53e9c84f94984cd04dfd16fc3d8878694dfb9
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-04-26 11:37:06 +0100

    class components

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-  const numbers = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30,
-  ];
-  const evenStyle = { color: "white", padding: "40px", backgroundColor: "red" };
-  const oddStyle = {
-    color: "white",
-    padding: "40px",
-    backgroundColor: "green",
-  };
-
-  return (
-    <div>
-      {numbers.map((number) =>
-        number % 2 == 0 ? (
-          <p style={evenStyle} key={number}>{number}</p>
-        ) : (
-          <p style={oddStyle} key={number}>{number}</p>
-        )
-      )}
-    </div>
-  );
-};
+class Header extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Class components</h1>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
