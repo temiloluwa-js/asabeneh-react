1990c62f2f6d49d66e2f8c727335cb582a3096b8
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-04-26 21:31:42 +0100

    final commit. form validations studied in React.

--- src/index.js
+++ src/index.js
@@ -1,81 +1,34 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-const Button = ({ text, onClick, style }) => (
-  <button style={style} onClick={onClick}>
-    {text}
-  </button>
-);
+const options = [
+  {
+    value: '',
+    label: '--Select Country--'
+  },
 
-// CSS styles in JavaScript Object
-const buttonStyles = {
-  backgroundColor: "#61dbfb",
-  padding: 10,
-  border: "none",
-  borderRadius: 5,
-  margin: "3px auto",
-  cursor: "pointer",
-  fontSize: 22,
-  color: "white",
-};
+  {
+    value: 'Finland',
+    label: 'Finland'
+  },
 
-// class based component
-class Header extends React.Component {
-  render() {
-    const {
-      welcome,
-      title,
-      subtitle,
-      author: { firstName, lastName },
-      date,
-    } = this.props.data;
+  {
+    value: 'Norway',
+    label: 'Norway'
+  },
 
-    return (
-      <header>
-        <div className="header-wrapper">
-          <h1>{welcome}</h1>
-          <h2>{title}</h2>
-          <h3>{subtitle}</h3>
-          <p>
-            {firstName} {lastName}
-          </p>
-          <small>{date}</small>
-        </div>
-      </header>
-    );
+  {
+    value: 'Denmark',
+    label: 'Denmark'
   }
-}
+]
 
-class App extends React.Component {
-  state = {
-    loggedIn: false,
-  };
+const selectOptions = options.map(({value, label}) => {
+  <option value={value}>{label}</option>
+})
 
-  handleClick = () => {
-    this.setState({
-      loggedIn: !this.state.loggedIn
-    });
-  };
-
-  render() {
-    const data = {
-      welcome: "30 Days Of React",
-      title: "Getting Started React",
-      subtitle: "JavaScript Library",
-      author: {
-        firstName: "Asabeneh",
-        lastName: "Yetayeh",
-      },
-      date: "Oct 9, 2020",
-    };
-
-    let status = this.state.loggedIn ? <h1>Hola mina-san</h1> : <h2>Log In First</h2>;
-    return <div className="home-wrapper">
-      <Header data={data}/>
-      {status}
-      <Button text={this.state.loggedIn ? 'Log Out': 'Log In'} style={buttonStyles} onClick={this.handleClick}/>
-    </div>;
-  }
+class App extends React.Component{
+  state
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
