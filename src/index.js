e7e9021b3e4b2ef4ff8fcf7ad20a7783a6304e45
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-04-26 11:56:02 +0100

    class components practice

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-class TechList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const techs = ["HTML", "CSS", "JavaScript"];
-    const techsformatted = techs.map((tech) => <li key={tech}>{tech}</li>);
-    return techsformatted
-  }
-}
-
-class Main extends React.Component{
+class Header extends React.Component{
   constructor(props){
     super(props)
   }
 
   render(){
-    <div className="main-wrapper">
-      <ul>
-        <TechList/>
-      </ul>
-    </div>
+    return (
+      <header>
+        <div className="header-wrapper">
+          <h1>{this.props.data.welcome}</h1>
+          <h2>{this.props.data.title}</h2>
+          <h3>{this.props.data.author.firstName} {this.props.data.author.lastName}</h3>
+          <small>{this.props.data.date}</small>
+        </div>
+      </header>
+    )
   }
+
+}
+
+const App = () => {
+  const data = {
+   welcome: 'Welcome to learning JS',
+   title: 'Getting Started React',
+   subtitle: 'JavaScript Library',
+   author: {
+     firstName: 'Asabeneh',
+     lastName: 'Yetayeh'
+   },
+   date: 'Oct 7, 2020'
+  }
+
+  return(
+    <div className="app">
+      <Header data={data}/>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
