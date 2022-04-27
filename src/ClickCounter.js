e1b6e31756ff41212b92582b3eeaa330cac28c24
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-04-27 11:17:02 +0100

    clickcounter

---
+++ src/ClickCounter.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class ClickCounter extends Component{
+    render(){
+        return(
+            <div>
+                <button>Clicked X times</button>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-
-class App extends Component {
-constructor(){
-  super();
-  this.state = {val: 0}
-  this.update = this.update.bind(this)
-}
-
-update(){
-  this.setState({val: this.state.val + 1})
-}
-
-componentWillMount(){
-  console.log('dada')
-}
-
-componentDidMount(){
-  
-}
-
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
